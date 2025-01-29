80cca6ea1c2999cbd03edd69919ffeec2b138374
Tony Sousa Lopes <167309815+tonysousa18@users.noreply.github.com>
2025-01-28 21:27:43 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
     define("HOST", "localhost");
     define("USER", "root");
-    define("PASS", "0704"); //segundo parametro: minha senha do xammpp
+    define("PASS", ""); 
     define("BASE", "cadastro");
 
 $conexao = new MySQLi(HOST, USER, PASS, BASE);
